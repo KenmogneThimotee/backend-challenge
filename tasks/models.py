96819263a73b8e4c61d7d50d685895e4403ab073
kenmognethimotee <kenmognethimotee@gmail.com>
2024-03-13 15:17:52 +0100

    Task and Label created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from user_management.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    priority = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['priority']
+        
+
+class Label(models.Model):
+    name = models.CharField(max_length=50)
+    tasks = models.ManyToManyField(Task)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
